30abb5a384c5dbea2723bd09f87141ed34bf0176
TouhruCreative <touhru.shott@gmail.com>
2026-09-26 02:10:31 +0500

    microupdate urls in users

--- backend/MentoriaEduAPI/users/urls.py
+++ backend/MentoriaEduAPI/users/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView, ProfileView
+
+from .views import RegisterView, MeView
+
 
 urlpatterns = [
- #   path("register/", RegisterView.as_view()),
- #   path("login/", TokenObtainPairView.as_view()),
- #   path("refresh/", TokenRefreshView.as_view()),
- #   path("profile/", ProfileView.as_view()),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("me/", MeView.as_view(), name="me"),
 ]
