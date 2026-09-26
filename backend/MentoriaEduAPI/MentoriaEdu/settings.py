7d0b9fc9a327091383b0f12e8ded40d7a62e282f
TouhruCreative <touhru.shott@gmail.com>
2026-09-26 05:01:59 +0500

    Last update

--- backend/MentoriaEduAPI/MentoriaEdu/settings.py
+++ backend/MentoriaEduAPI/MentoriaEdu/settings.py
@@ -138,4 +138,5 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
